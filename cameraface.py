afc787527433c0d7555c90d6d537520c94137e82
ElieDDD <30892423+ElieDDD@users.noreply.github.com>
2025-01-07 10:21:43 +0000

    Update cameraface.py

--- cameraface.py
+++ cameraface.py
@@ -30,7 +30,7 @@ image_files = [f for f in os.listdir(folder_path) if f.endswith(('jpg', 'jpeg', 
 st.markdown("### This is a row of faces")
 
 # Define the number of columns in the grid
-num_columns = 10  # You can change this value based on how many columns you want
+num_columns = 15  # You can change this value based on how many columns you want
 
 # Create columns for the grid
 columns = st.columns([1] * num_columns)
